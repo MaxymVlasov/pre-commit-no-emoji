c3571cfed923d5e0ece3157941bc69228a1ef272
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2025-01-07 22:54:51 +0200

    Populate files to hook

--- src/pre_commit_no_emoji/__main__.py
+++ src/pre_commit_no_emoji/__main__.py
@@ -5,5 +5,5 @@ from sys import argv, exit as exit_with_return_code
 from .remove_emoji import invoke_cli_app
 
 
-return_code = invoke_cli_app(argv[1:])
+return_code = invoke_cli_app(argv[2:])
 exit_with_return_code(return_code)
